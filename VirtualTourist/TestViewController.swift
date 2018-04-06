0bd48b163d084947d646325bba915de8c51568f1
Timothy Ng <ncy.timothy@gmail.com>
2018-04-06 12:37:57 -0700

    MapView, Delete Prompt with Label running properly

--- VirtualTourist/TestViewController.swift
+++ VirtualTourist/TestViewController.swift
@@ -11,6 +11,7 @@ import MapKit
 
 class TestViewController: UIViewController {
     
+    // MARK: - Properties
     var mapViewIsShift = false
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var deletePromptView: UIView!
@@ -23,10 +24,25 @@ class TestViewController: UIViewController {
         deletePromptView.frame.size.width = view.frame.size.width
     }
     
+    fileprivate func configureDeleteLabel() {
+        /**
+        Create a label for the delete pins prompt
+        * Initially defining the label's frame is merely for initialization
+        */
+        let labelRect = CGRect(x: 0, y: 0, width: 200, height: 21)
+        label = UILabel(frame: labelRect)
+        label.font = UIFont.preferredFont(forTextStyle: .headline)
+        label.text = "Tap Pins to Delete"
+        label.textColor = .white
+        self.label.textAlignment = .center
+        self.view.addSubview(self.label)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.rightBarButtonItem = self.editButtonItem
         configureDeletePrompt()
+        configureDeleteLabel()
     }
 
     override func didReceiveMemoryWarning() {
@@ -52,58 +68,17 @@ class TestViewController: UIViewController {
         let promptHeight = deletePromptView.frame.size.height
         let promptWidth = deletePromptView.frame.size.width
         
-        let labelRect = CGRect(x: promptX, y: promptY, width: promptWidth, height: promptHeight)
-        label = UILabel(frame: labelRect)
-        label.text = "Veronica I Love You!"
-        label.textColor = UIColor(named: "White")
-        
-        
-        
         UIView.animate(withDuration: 0.1, animations: {
             if self.mapViewIsShift {
                 self.mapView.frame = CGRect(x: mapX, y: shiftMapY, width: mapWidth, height: mapHeight)
                 self.deletePromptView.frame = CGRect(x: promptX, y: shiftPromptY, width: promptWidth, height: promptHeight)
                 self.label.frame = CGRect(x: promptX, y: shiftPromptY, width: promptWidth, height: promptHeight)
-                self.label.center = self.deletePromptView.center
-                self.label.textAlignment = .center
-                self.deletePromptView.addSubview(self.label)
             } else {
                 self.mapView.frame = CGRect(x: mapX, y: mapY, width: mapWidth, height: mapHeight)
                 self.deletePromptView.frame = CGRect(x: promptX, y: promptY, width: promptWidth, height: promptHeight)
+                self.label.frame = CGRect(x: promptX, y: promptY, width: promptWidth, height: promptHeight)
             }
             
         })
     }
-    
-//    @IBAction func verPressed(_ sender: Any) {
-//        mapViewIsShift = !mapViewIsShift
-//        print("mapViewShift: \(mapViewIsShift)")
-//
-//        let mapX = mapView.frame.origin.x
-//        let mapY = mapView.frame.origin.y + 100
-//        let shiftMapY = mapView.frame.origin.y - 100
-//        let promptX = deletePromptView.frame.origin.x
-//        let promptY = view.frame.size.height
-//        let shiftPromptY = deletePromptView.frame.origin.y - 100
-//
-//
-//        let mapHeight = mapView.frame.size.height
-//        let mapWidth = mapView.frame.size.width
-//        let promptHeight = deletePromptView.frame.size.height
-//        let promptWidth = deletePromptView.frame.size.width
-//
-//        UIView.animate(withDuration: 0.1, animations: {
-//            if self.mapViewIsShift {
-//                self.mapView.frame = CGRect(x: mapX, y: shiftMapY, width: mapWidth, height: mapHeight)
-//                self.deletePromptView.frame = CGRect(x: promptX, y: shiftPromptY, width: promptWidth, height: promptHeight)
-//
-//            } else {
-//                self.mapView.frame = CGRect(x: mapX, y: mapY, width: mapWidth, height: mapHeight)
-//                self.deletePromptView.frame = CGRect(x: promptX, y: promptY, width: promptWidth, height: promptHeight)
-//            }
-//
-//        })
-//
-//    }
-    
 }
